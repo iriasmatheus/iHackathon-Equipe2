9ceffd390d6677d2007fb81bd91f6dfe91b81fd1
VictorAugustoqwe <vaugustocruz22@gmail.com>
2021-04-18 15:26:01 -0300

    Final Commit

--- birthdaybot.py
+++ birthdaybot.py
@@ -21,7 +21,7 @@ def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     #schedule jobs
     if len(context.job_queue.jobs()) == 0:
-        scheduledtime = datetime.time(hour=1, minute=12, tzinfo=pytz.timezone('America/Sao_Paulo'))
+        scheduledtime = datetime.time(hour=15, minute=50, tzinfo=pytz.timezone('America/Sao_Paulo'))
         context.job_queue.run_daily(callbackreminder, scheduledtime, days=(0, 1, 2, 3, 4, 5, 6), context=update.message.chat_id)
         update.message.reply_text('Fui iniciado!\nIrei avisar quando alguém fizer aniversário :)')
     else:
@@ -31,6 +31,11 @@ def help_command(update: Update, _: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Opa, obrigado por me usar :), fale comigo da seguinte forma\n/start - Usado quando colocar o bot no canal\n/meuaniversario dd/mm/aaaa - Cadastra seu aniversário\n/listaraniversarios - Lista todos os aniversários cadastrados\n/delete - Deleta o cadastro do seu aniversário')
    
+def inicia_arquivo() -> None:
+    with open('birthdays.csv', 'w', newline='') as aniversarios:
+        fieldnames = ['id', 'nome', 'data']
+        writer = csv.DictWriter(aniversarios, fieldnames=fieldnames)
+        writer.writeheader()
 
 def delete(user_id) -> "deleted":
     """Delete user and return if one was deleted"""
@@ -63,45 +68,58 @@ def delete_user(update: Update, context: CallbackContext) -> None:
 
 def my_birthday(update: Update, context: CallbackContext) -> None:
     """Write user birthday in csv"""
-    if len(context.args) > 0:
+    try:
+        if len(context.args) == 0:
+            raise Exception('Sem argumentos')
         aniversario = context.args[0]
-        try:
-            datetime.datetime.strptime(aniversario, '%d/%m/%Y')
-            user = update.message.from_user
-            user_id = str(user.id)
-            user_name = user.full_name
-            user_birthday = aniversario
-            delete(user_id)
-            with open('birthdays.csv', 'a', newline='') as aniversarios:
-                fieldnames = ['id','nome', 'data']
-                writer = csv.DictWriter(aniversarios, fieldnames=fieldnames)
-                writer.writerow({'id': user_id, 'nome': user_name, 'data': user_birthday})
-            update.message.reply_text('Olá, ' + user_name + "! Eu vou lembrar do seu grande dia!")
-        except ValueError:
-            update.message.reply_text('Data no formato incorreto!')
-    else:
-        update.message.reply_text('Você deve informar uma data!')
+        datetime.datetime.strptime(aniversario, '%d/%m/%Y')
+        user = update.message.from_user
+        user_id = str(user.id)
+        user_name = user.full_name
+        user_birthday = aniversario
+        today = datetime.date.today()
+        formated_date = today.strftime("%d/%m/%Y")
+        if(int(aniversario[6:]) > int(formated_date[6:])):
+            raise Exception('Data de aniversário no futuro')
+        delete(user_id)
+        with open('birthdays.csv', 'a', newline='') as aniversarios:
+            fieldnames = ['id','nome', 'data']
+            writer = csv.DictWriter(aniversarios, fieldnames=fieldnames)
+            writer.writerow({'id': user_id, 'nome': user_name, 'data': user_birthday})
+        update.message.reply_text('Olá, ' + user_name + '! Eu vou lembrar do seu grande dia!')
+    except ValueError:
+        update.message.reply_text('Data no formato incorreto!')
+    except Exception as error:
+        if str(error) == 'Data de aniversário no futuro':
+            update.message.reply_text('Você veio do futuro??\nCheque o ano de seu aniversário')
+        elif str(error) == 'Sem argumentos':
+            update.message.reply_text('Você deve informar uma data!')
 
 def list_birthdays(update: Update, context: CallbackContext) -> None:
     """List user birthdays in csv"""
-    update.message.reply_text("Os aniversários que eu sei são:\n")
-    with open('birthdays.csv', newline='') as aniversarios:
-        reader = csv.DictReader(aniversarios)
-        aniversarioslist = []
-        for row in reader:
-            aniversarioslist.append(row['nome'] + " - " + row['data'] + "\n")
-    aniversariospormensagem = 5
-    for nmensagens in range(math.floor(len(aniversarioslist)/aniversariospormensagem)):
-        mensagem = ""
-        for pormensagem in range(aniversariospormensagem):
-            mensagem = mensagem + aniversarioslist[5*nmensagens + pormensagem]
-        context.bot.send_message(chat_id= update.effective_chat.id, text= mensagem)
+    try:
+        with open('birthdays.csv', newline='') as aniversarios:
+            reader = csv.DictReader(aniversarios)
+            aniversarioslist = []
+            for row in reader:
+                aniversarioslist.append(row['nome'] + " - " + row['data'] + "\n")
+        if(len(aniversarioslist) == 0):
+            raise Exception('Nenhum aniversário cadastrado!!')
+        update.message.reply_text("Os aniversários que eu sei são:\n")
+        aniversariospormensagem = 5
+        for nmensagens in range(math.floor(len(aniversarioslist)/aniversariospormensagem)):
+            mensagem = ""
+            for pormensagem in range(aniversariospormensagem):
+                mensagem = mensagem + aniversarioslist[5*nmensagens + pormensagem]
+            context.bot.send_message(chat_id= update.effective_chat.id, text= mensagem)
 
-    nultimasmensagens = len(aniversarioslist) % aniversariospormensagem
-    mensagem = ""
-    for ultimasmensagens in range(nultimasmensagens):
-        mensagem = mensagem + aniversarioslist[-nultimasmensagens+ultimasmensagens]
-    context.bot.send_message(chat_id= update.effective_chat.id, text= mensagem)
+        nultimasmensagens = len(aniversarioslist) % aniversariospormensagem
+        mensagem = ""
+        for ultimasmensagens in range(nultimasmensagens):
+            mensagem = mensagem + aniversarioslist[-nultimasmensagens+ultimasmensagens]
+        context.bot.send_message(chat_id= update.effective_chat.id, text= mensagem)
+    except Exception as error:
+        context.bot.send_message(chat_id= update.effective_chat.id, text= "Não há nenhum aniversário cadastrado :(")
 
 
 def callbackreminder(context) -> None:
@@ -124,7 +142,10 @@ def main() -> None:
     updater = Updater(token)
 
     #create birthdays.csv if it doesn't exist or is wrong
-    delete("-1")
+    try:
+        delete("-1")
+    except FileNotFoundError:
+        inicia_arquivo()
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
